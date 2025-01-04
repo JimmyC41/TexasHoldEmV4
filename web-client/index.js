77f6f205d26f772f979f83dc4bf93e937c12f1e4
Jimmy Chan <z5478376@ad.unsw.edu.au>
2025-01-04 21:56:55 +1100

    Implementation of joinGame in client-side

--- web-client/index.js
+++ web-client/index.js
@@ -1,19 +1,26 @@
-const grpcProto = require('./generated/texas_holdem_grpc_web_pb.js');
-const proto = require('./generated/texas_holdem_pb.js');
+const grpc_pb = require('./generated/texas_holdem_grpc_web_pb.js');
+const pb = require('./generated/texas_holdem_pb.js');
 
-console.log('grpcProto:', grpcProto);
-console.log('proto:', proto);
+const client = new grpc_pb.PokerServicePromiseClient('http://localhost:8080');
 
-const client = new grpcProto.PokerServicePromiseClient('http://localhost:8080');
-const joinGameReq = new proto.JoinGameReq();
+/**
+ * Sends a JoinGame request to the gRPC server.
+ * @param {string} playerName - The name of the player.
+ * @param {number} chips - The initial chip count for the player.
+ * @returns {Promise<Object>} - A promise resolving to the response object.
+ */
+const joinGame = (playerName, chips) => {
+    return new Promise((resolve, reject) => {
+        const joinGameReq = new pb.JoinGameReq();
+        joinGameReq.setPlayerName(playerName);
+        joinGameReq.setChips(chips);
 
-joinGameReq.setPlayerName('Jimmy');
-joinGameReq.setChips(1000);
-
-client.joinGame(joinGameReq)
-    .then((response) => {
-        console.log('Join Game Response', response.toObject())
-    })
-    .catch((error) => {
-        console.log('Error:', error.message);
+        client.joinGame(joinGameReq)
+            .then((response) => {
+                resolve(response.toObject());
+            })
+            .catch((error) => {
+                reject(error.message);
+            });
     });
+};
