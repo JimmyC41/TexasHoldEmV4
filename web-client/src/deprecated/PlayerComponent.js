2e8cb1742d040145e15639d9d70020c2f335678b
Jimmy Chan <z5478376@ad.unsw.edu.au>
2025-01-08 15:54:56 +1100

    Added a deprecated folder for old ui components

--- web-client/src/deprecated/PlayerComponent.js
+++ web-client/src/deprecated/PlayerComponent.js
@@ -1,6 +1,7 @@
+// PlayerComponent.js
 import React from 'react';
 
-const PlayerComponent = ({ player, isNextToAct }) => {
+const PlayerComponent = ({ player, isNextToAct, isDealer }) => {
     const playerStyles = {
         display: 'flex',
         flexDirection: 'column',
@@ -12,13 +13,24 @@ const PlayerComponent = ({ player, isNextToAct }) => {
         width: '120px',
         textAlign: 'center',
     };
-    
+
     return (
         <div style={playerStyles}>
             <div>{player.name}</div>
+            {isDealer && <div style={dealerButtonStyle}>D</div>}
             <div>Chips: {player.chips}</div>
+            <div>Recent Bet: {player.recentBets}</div>
         </div>
     );
+};
+
+const dealerButtonStyle = {
+    backgroundColor: 'gold',
+    color: 'black',
+    fontWeight: 'bold',
+    borderRadius: '50%',
+    padding: '5px',
+    marginBottom: '5px',
 };
 
 export default PlayerComponent;
