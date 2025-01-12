f5e4ce7baf1c54e3defe25a1fa0a963029f3bb23
Jimmy Chan <z5478376@ad.unsw.edu.au>
2025-01-12 14:06:27 +1100

    Removed bullet points on home pg

--- web-client/src/pages/HomePage.js
+++ web-client/src/pages/HomePage.js
@@ -18,24 +18,6 @@ const HomePage = () => {
             <div className="central-content">
 				<h1 className="page-title">Texas Hold'em</h1>
                 <JoinGameComponent dispatch={dispatch} navigate={navigate} />
-                <div className="design-explanations">
-                    <h4>
-                        <a 
-                            href="https://github.com/JimmyC41" 
-                            target="_blank" 
-                            rel="noopener noreferrer"
-                            className="github-link"
-                        >
-                            https://github.com/JimmyC41
-                        </a>
-                    </h4>
-                    <ul>
-                        <li>Description</li>
-                        <li>Backend Features</li>
-                        <li>Frontend Features</li>
-                        <li>Game Rules</li>
-                    </ul>
-                </div>
             </div>
         </div>
     );
